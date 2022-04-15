74ab7eb6f119aba161bbbfbfc5277376a11abaed
Zhiw106 <zhiwei173@yahoo.com>
2022-04-15 12:46:22 -0400

    complete chatServer and chatApp

---
+++ chatApp/src/app/chat.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  private socket: Socket | undefined; // The client instance of socket.io
+  public getMessages: Subject<string> = new Subject();
+
+  constructor() {
+    this.socket = io('http://localhost:8080');
+
+    this.socket?.on('chat message', (msg: string) => {
+      this.getMessages.next(msg); // send the new message
+    });
+  }
+
+  sendMessage(msg: string) {
+    this.socket?.emit('chat message', msg);
+  }
+}
